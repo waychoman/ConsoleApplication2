1dab12d8dbe3a6c202249fae12e082909b61e03e
4D-25\koyo <a>
2026-08-25 13:37:02 +0900

    a

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,4 +5,19 @@ int max_int(int a, int b) {
 	return a > b ? a : b;
 }
 
-int max_long()
+long max_long(long a, long b) {
+	return a > b ? a : b;
+}
+
+float max_float(float a, float b) {
+	return a > b ? a : b;
+}
+
+int main() {
+	float a = 10.9;
+		float b = 10.;
+
+		std::cout << "Max: " << max_int(a, b) << std::endl;
+		std::cout << "Max: " << max_long(a, b) << std::endl;
+		std::cout << "Max: " << max_float(a, b) << std::endl;
+}

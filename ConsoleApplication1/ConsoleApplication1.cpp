c6b8ceee3c786c9c821aabeebb31541100f651d7
4D-25\koyo <a>
2026-08-25 15:24:51 +0900

    const

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -25,10 +25,21 @@ template<typename H, std::size_t a>struct IsArray<H[a]>
 	static constexpr bool value = true;
 };
 
+template<typename C>struct IsConst
+{
+	static constexpr bool value = false;
+};
+
+template<typename C>struct IsConst<const C >
+{
+	static constexpr bool value = true;
+};
+
 int main()
 {
-	std::cout << IsArray<int>::value << std::endl;
-	std::cout << IsArray<int[10]>::value << std::endl;
-	std::cout << IsArray<float[5]>::value << std::endl;
+	std::cout << IsConst<int>::value << std::endl;
+	std::cout << IsConst<const int>::value << std::endl;
+	std::cout << IsConst<float>::value << std::endl;
+	std::cout << IsConst<const float>::value << std::endl;
 
 }
